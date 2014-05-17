c68a33be9f1c95fb453f0b5bbb873bf2dd35129b
xkurnx <xkurnx@gmail.com>
2014-05-17 13:18:03 +0700

    dev-config-tinyMCE

--- functions.php
+++ functions.php
@@ -141,10 +141,38 @@ add_post_type_support('page', 'excerpt');
 
 if ( ! function_exists('tdav_css') ) {
 	function tdav_css($wp) {
-		$wp .= ',' . get_template_directory_uri() . '/css/style.css';
+		$wp .= ',' . get_template_directory_uri() . '/css/index.css';
+		$wp .= ',' . get_template_directory_uri() . '/css/site_global.css';
 	return $wp;
 	}
 }
 add_filter( 'mce_css', 'tdav_css' );
+
+
+function myformatTinyMCE($in)
+{
+ $in['remove_linebreaks']=false;
+ $in['gecko_spellcheck']=false;
+ $in['keep_styles']=true;
+ $in['accessibility_focus']=true;
+ $in['tabfocus_elements']='major-publishing-actions';
+ $in['media_strict']=false;
+ $in['paste_remove_styles']=false;
+ $in['paste_remove_spans']=false;
+ $in['paste_strip_class_attributes']='none';
+ $in['paste_text_use_dialog']=true;
+ $in['wpeditimage_disable_captions']=true;
+ $in['plugins']='tabfocus,paste,media,fullscreen,wordpress,wpeditimage,wpgallery,wplink,wpdialogs,wpfullscreen';
+ $in['content_css']=  get_template_directory_uri() . '/css/index.css'.',' . get_template_directory_uri() . '/css/site_global.css';
+ $in['wpautop']=false;
+ $in['apply_source_formatting']=true;
+ $in['toolbar1']='bold,italic,strikethrough,bullist,numlist,blockquote,hr,alignleft,aligncenter,alignright,link,unlink,wp_more,spellchecker,wp_fullscreen,wp_adv ';
+ $in['toolbar2']='formatselect,underline,alignjustify,forecolor,pastetext,removeformat,charmap,outdent,indent,undo,redo,wp_help ';
+ $in['toolbar3']='';
+ $in['toolbar4']='';
+ return $in;
+}
+add_filter('tiny_mce_before_init', 'myformatTinyMCE' );
+
 	
 ;?>
